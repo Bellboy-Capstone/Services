9322cbdc6f156b272039609c3a31f3e5a91116d5
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-09-27 12:06:56 -0600

    Format some tests and migrations.

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["bellboy-services.herokuapp.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["bellboy-services.herokuapp.com"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -83,14 +85,13 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Services <noreply@bellboy-services.herokuapp.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="Services <noreply@bellboy-services.herokuapp.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[Services]"
-)
+EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[Services]")
 
 # ADMIN
 # ------------------------------------------------------------------------------
