8c12e26b21948d4ca6f285a2258b36d0d2533d33
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-04 23:27:32 -0700

    Add Bellboy registration endpoint.

--- services/bellboy/urls.py
+++ services/bellboy/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from services.bellboy.views import BellboyDeviceViewSet, StatusUpdateViewSet
+from services.bellboy.views import (
+    BellboyDeviceViewSet,
+    BellboyRegistrationView,
+    StatusUpdateViewSet,
+)
 
 app_name = "bellboy"
 
 router = DefaultRouter()
-router.register(r"device", BellboyDeviceViewSet, basename="device")
-router.register(r"status-update", StatusUpdateViewSet, basename="status-update")
+router.register(r"devices", BellboyDeviceViewSet, basename="device")
+router.register(r"status-updates", StatusUpdateViewSet, basename="status-update")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("admin/", include(router.urls)),
+    path("register-device/", view=BellboyRegistrationView.as_view(), name="register"),
+]
--- services/bellboy/views.py
+++ services/bellboy/views.py
@@ -1,5 +1,7 @@
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from services.bellboy.models import BellboyDevice, StatusUpdate
@@ -22,3 +24,30 @@ class StatusUpdateViewSet(ModelViewSet):
     authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = StatusUpdate.objects.all()
+
+
+class BellboyRegistrationView(APIView):
+
+    """Deploys a simple POST endpoint for Bellboy devices."""
+
+    permission_classes = [AllowAny]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
+    # def post(self, request, format=None):
+    #     """Allows a bellboy to ensure the Services are up and responding."""
+    #     serializer = BellboyDeviceSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     data = serializer.validated_data
+    #     return Response({"status": "OK", "received": data})
+
+    def get(self, request=None):
+        """Creates a new Bellboy device and returns an identifier."""
+        new_bellboy = BellboyDevice()
+        new_bellboy.save()
+        return Response(
+            {
+                "status": "OK",
+                "message": "New Bellboy device created.",
+                "identifier": new_bellboy.identifier,
+            }
+        )
