86b1fa7cc45d49e66320600330aba265181ff9c1
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-04 23:14:14 -0700

    Use correct permission class in viewsets.

--- services/bellboy/views.py
+++ services/bellboy/views.py
@@ -1,12 +1,9 @@
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from services.bellboy.models import BellboyDevice, StatusUpdate
 from services.bellboy.serializers import BellboyDeviceSerializer, StatusUpdateSerializer
-
-
-class IsAuthenticatedOrReadonly(object):
-    pass
 
 
 class BellboyDeviceViewSet(ModelViewSet):
@@ -14,7 +11,7 @@ class BellboyDeviceViewSet(ModelViewSet):
 
     serializer_class = BellboyDeviceSerializer
     authentication_classes = [TokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticatedOrReadonly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = BellboyDevice.objects.all()
 
 
@@ -23,5 +20,5 @@ class StatusUpdateViewSet(ModelViewSet):
 
     serializer_class = StatusUpdateSerializer
     authentication_classes = [TokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticatedOrReadonly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = StatusUpdate.objects.all()
