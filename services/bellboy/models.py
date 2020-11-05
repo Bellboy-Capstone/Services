dc5eb7b1a5fa7bab72981649f54f980fbe9a63b2
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-04 23:01:09 -0700

    feat: Add endpoints for new Bellboy models.

--- services/bellboy/models.py
+++ services/bellboy/models.py
@@ -13,13 +13,18 @@ class BellboyDevice(Model):
 
     # Generate a unique key for the Bellboy unit to keep.
     # Better not to use the model's ID for this.
-    identifier = UUIDField(default=uuid.uuid4, editable=False)
+    identifier = UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
 
 class StatusUpdate(Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # The identifier of the bellboy associated with the status update.
     bellboy = ForeignKey(
-        BellboyDevice, related_name="status_updates", on_delete=CASCADE
+        BellboyDevice,
+        related_name="status_updates",
+        on_delete=CASCADE,
+        to_field="identifier",
     )
     created = DateTimeField(auto_now_add=True, editable=False)
 
