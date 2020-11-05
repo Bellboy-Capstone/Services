5d97ad2169bd4ed197e58a6d430559d920833ef3
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-04 21:55:41 -0700

    Add GET endpoint to heartbeat app.

--- services/heartbeat/views.py
+++ services/heartbeat/views.py
@@ -5,11 +5,16 @@ from services.heartbeat.serializers import HeartbeatSerializer
 
 
 class HeartbeatView(APIView):
-    """Deploys a simple POST endpoint for Bellboy devices"""
+    """Deploys a simple POST endpoint for Bellboy devices."""
 
     def post(self, request, format=None):
-        """Allows a bellboy to ensure the Services are up and responding"""
+        """Allows a bellboy to ensure the Services are up and responding."""
         serializer = HeartbeatSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
         return Response({"status": "OK", "received": data})
+
+    def get(self, request=None):
+        return Response(
+            {"status": "OK", "message": "Bellboy Services are up and ready to go!"}
+        )
