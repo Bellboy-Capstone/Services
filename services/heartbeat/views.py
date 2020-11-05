fa8a91851fe43367ff26220a2a176f7a93bf86e6
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-04 22:02:55 -0700

    Change permission classes on HeartbeatView

--- services/heartbeat/views.py
+++ services/heartbeat/views.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,7 +7,11 @@ from services.heartbeat.serializers import HeartbeatSerializer
 
 
 class HeartbeatView(APIView):
+
     """Deploys a simple POST endpoint for Bellboy devices."""
+
+    permission_classes = [AllowAny]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
 
     def post(self, request, format=None):
         """Allows a bellboy to ensure the Services are up and responding."""
